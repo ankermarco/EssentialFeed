1dfe5bc23162a5ae731c8b1ab478b4b65fbab735
Ke Ma <kema2012@gmail.com>
2021-09-15 21:46:35 +0100

    Save command requests cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //  Copyright © 2021 Ke Ma. All rights reserved.
 //
 
+import EssentialFeed
 import XCTest
+
 class LocalFeedLoader {
+    private let store: FeedStore
     
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 class FeedStore {
-    var deleteCachedFeedCallCount = 0
+    private(set) var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 class CacheFeedUseCaseTests: XCTestCase {
 
@@ -22,5 +33,21 @@ class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem, uniqueItem]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    private var anyURL: URL {
+        URL(string: "http://any-url.com")!
+    }
+    
+    private var uniqueItem: FeedItem {
+        FeedItem(id: UUID(), description: "", location: "", imageURL: anyURL)
     }
 }
