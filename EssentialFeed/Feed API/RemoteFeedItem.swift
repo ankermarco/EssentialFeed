7b6da4fb8eceb83a2f234b981f45d36ecd735ae7
Ke Ma <kema2012@gmail.com>
2021-10-03 21:00:49 +0100

    Move `RemoteFeedItem` to its own file

---
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ke Ma on 03/10/2021.
+//  Copyright © 2021 Ke Ma. All rights reserved.
+//
+
+import Foundation
+
+struct RemoteFeedItem: Decodable {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
